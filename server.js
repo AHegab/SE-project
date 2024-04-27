beea2bc31acdf33806a62c94764f58ca7e662b94
AHegab <amhegab305@gmail.com>
2024-04-27 22:26:31 +0300

    10:26

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const app=express();
 
 const port=process.env.PORT || 3001;
 
+require('dotenv').config();
+
 app.listen(port,()=>{
 
-    console.log("working");
+    console.log("server is listening to port: "+ port);
+})
+
+
+app.get("/v1/api/AdminRole",(req,res)=>{
+    res.send("fetching");
+    console.log("working")
 })
