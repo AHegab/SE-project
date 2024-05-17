3266d444e4ad8823b60077f232c06f35c5ccb5f9
omarsuff <omarwaleedalazhary@gmail.com>
2024-05-17 14:54:09 +0300

    profile user information
    sign out still required

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 import Home from './routes/Home';
 import Login from './routes/Login';
+import Profile from './routes/Profile';
+
 import Header from './Header.js';
 import Footer from './Footer.js';
 
@@ -14,6 +16,8 @@ const App = ()=> {
           <Routes>
           <Route path='/' exact Component={Home}></Route>
           <Route path='/login' exact Component={Login}></Route>
+          <Route path='/Profile' exact Component={Profile}></Route>
+
           </Routes>
           <Footer />
         </Router>
